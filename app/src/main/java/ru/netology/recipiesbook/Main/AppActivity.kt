2143af2293fd228e45f31ea4966a90c53a3a39bd
QBush <ignor-KuBush@yandex.ru>
2022-08-06 21:58:49 +1000

    Нижнее меню плюс-минус готово

--- app/src/main/java/ru/netology/recipiesbook/Main/AppActivity.kt
+++ app/src/main/java/ru/netology/recipiesbook/Main/AppActivity.kt
@@ -1,6 +1,22 @@
 package ru.netology.recipiesbook.Main
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.netology.recipiesbook.R
 
-class AppActivity : AppCompatActivity(R.layout.app_activity)
+class AppActivity : AppCompatActivity(R.layout.app_activity) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bottom_nav)
+            .setupWithNavController(navController)
+    }
+
+}
